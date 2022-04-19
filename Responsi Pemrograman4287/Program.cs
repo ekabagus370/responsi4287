cc66b728455a4f33aa1217fc80ca033d34808f92
ekabagus370 <ekabaguserlangga370@amikom.ac.id>
2022-04-19 16:36:11 +0700

    responsi4287

---
+++ Responsi Pemrograman4287/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ResponsiPemrograman4308
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan("190302123", "dana", 4000000);
+            Karyawan karyawan2 = new Karyawan("190302124", "eka", 2000000);
+
+            Console.WriteLine("No Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.Write("1 "); karyawan1.tampilkanData();
+            Console.Write("2 "); karyawan2.tampilkanData();
+            Console.WriteLine();
+
+            karyawan1.naikGaji();
+            karyawan2.naikGaji();
+
+            Console.WriteLine();
+            Console.WriteLine("No Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.Write("1 "); karyawan1.tampilkanData();
+            Console.Write("2 "); karyawan2.tampilkanData();
+        }
+    }
+}
